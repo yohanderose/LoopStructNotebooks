cbf34cb5c6d147208a532c20fad153d2fae649ef
Yohan de Rose <yohanderose@gmail.com>
2019-11-18 21:53:49 +1100

    all bugs fixed

--- banner_magics.py
+++ banner_magics.py
@@ -7,15 +7,18 @@ Use `%top_banner` and `%bottom_banner`, without backticks, in separate code cell
 """
 
 from IPython.core.magic import register_line_magic
-from IPython.display import SVG, Image, HTML
+from IPython.display import SVG, Image
+
+import os 
+dir_path = os.path.dirname(os.path.realpath(__file__))
 
 @register_line_magic
 def top_banner(line):
-    r = Image("loop-struct-header.png")
+    r = Image(dir_path + "/loop-struct-header.png")
     display(r)
     
 @register_line_magic
 def bottom_banner(line):
-    r = Image("loop-struct-foot.png")
+    r = Image(dir_path + "/loop-struct-foot.png")
     display(r)
     
